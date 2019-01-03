6fd0eab023c4c9fca0de3040af7dcc1f0917eeb8
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-03 10:18:25 +0330

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.cToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.dToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.eToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.b1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.b2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.d1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.d2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.d3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aToolStripMenuItem,
+            this.bToolStripMenuItem,
+            this.cToolStripMenuItem,
+            this.dToolStripMenuItem,
+            this.eToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(82, 114);
+			// 
+			// aToolStripMenuItem
+			// 
+			this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+			this.aToolStripMenuItem.Size = new System.Drawing.Size(81, 22);
+			this.aToolStripMenuItem.Text = "a";
+			// 
+			// bToolStripMenuItem
+			// 
+			this.bToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.b1ToolStripMenuItem,
+            this.b2ToolStripMenuItem});
+			this.bToolStripMenuItem.Name = "bToolStripMenuItem";
+			this.bToolStripMenuItem.Size = new System.Drawing.Size(81, 22);
+			this.bToolStripMenuItem.Text = "b";
+			// 
+			// cToolStripMenuItem
+			// 
+			this.cToolStripMenuItem.Name = "cToolStripMenuItem";
+			this.cToolStripMenuItem.Size = new System.Drawing.Size(81, 22);
+			this.cToolStripMenuItem.Text = "c";
+			// 
+			// dToolStripMenuItem
+			// 
+			this.dToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.d1ToolStripMenuItem,
+            this.d2ToolStripMenuItem,
+            this.d3ToolStripMenuItem});
+			this.dToolStripMenuItem.Name = "dToolStripMenuItem";
+			this.dToolStripMenuItem.Size = new System.Drawing.Size(81, 22);
+			this.dToolStripMenuItem.Text = "d";
+			// 
+			// eToolStripMenuItem
+			// 
+			this.eToolStripMenuItem.Name = "eToolStripMenuItem";
+			this.eToolStripMenuItem.Size = new System.Drawing.Size(81, 22);
+			this.eToolStripMenuItem.Text = "e";
+			// 
+			// b1ToolStripMenuItem
+			// 
+			this.b1ToolStripMenuItem.Name = "b1ToolStripMenuItem";
+			this.b1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.b1ToolStripMenuItem.Text = "b1";
+			// 
+			// b2ToolStripMenuItem
+			// 
+			this.b2ToolStripMenuItem.Name = "b2ToolStripMenuItem";
+			this.b2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.b2ToolStripMenuItem.Text = "b2";
+			// 
+			// d1ToolStripMenuItem
+			// 
+			this.d1ToolStripMenuItem.Name = "d1ToolStripMenuItem";
+			this.d1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.d1ToolStripMenuItem.Text = "d1";
+			// 
+			// d2ToolStripMenuItem
+			// 
+			this.d2ToolStripMenuItem.Name = "d2ToolStripMenuItem";
+			this.d2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.d2ToolStripMenuItem.Text = "d2";
+			// 
+			// d3ToolStripMenuItem
+			// 
+			this.d3ToolStripMenuItem.Name = "d3ToolStripMenuItem";
+			this.d3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.d3ToolStripMenuItem.Text = "d3";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.ContextMenuStrip = this.contextMenuStrip1;
+			this.button2.Location = new System.Drawing.Point(12, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.ContextMenuStrip = this.contextMenuStrip1;
+			this.button4.Location = new System.Drawing.Point(12, 99);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(403, 308);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem bToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem b1ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem b2ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem cToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem dToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem d1ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem d2ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem d3ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem eToolStripMenuItem;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
